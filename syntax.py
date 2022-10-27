74c03a1136b94fc732e66ea043b334ec46417263
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2022-10-27 22:01:01 +0000

    10/27

---
+++ syntax.py
@@ -0,0 +1,26 @@
+print("Hello World")
+# \n skips a line
+# ''' or """ makes multiline comments
+print(2*"\n")
+v1,v2,v3="red","white","blue"
+print(v1+" "+v2+" "+v3)
+colors = ["red", "white", "blue"]
+x,y,z = colors
+someFloat=17.9
+print(type(someFloat))
+print(2*"\n")
+print("Is the number greater than 1?")
+import random
+p=random.randrange(1,100)/random.randrange(1,100)
+print(p)
+if p>1 : print ("yes")
+else : print("no")
+
+greeting="Hello"
+print(greeting[2])
+print(greeting[0])
+print(len(greeting))
+
+txt = "October"
+print(txt[1:4])
+
