3c89fbd80c3762d2f0ebe5ef9300a7ca8424a8d1
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2022-11-01 22:00:00 +0000

    11.01

--- syntax.py
+++ syntax.py
@@ -1,6 +1,7 @@
 print("Hello World")
 # \n skips a line
 # ''' or """ makes multiline comments
+#In class coding
 print(2*"\n")
 v1,v2,v3="red","white","blue"
 print(v1+" "+v2+" "+v3)
@@ -13,14 +14,47 @@ print("Is the number greater than 1?")
 import random
 p=random.randrange(1,100)/random.randrange(1,100)
 print(p)
-if p>1 : print ("yes")
+if p>1 : print("yes")
 else : print("no")
-
+print(2*"\n")
 greeting="Hello"
 print(greeting[2])
 print(greeting[0])
 print(len(greeting))
 
 txt = "October"
-print(txt[1:4])
+print(txt[-2:6])
+print(2*"\n")
+someTxt="Red is my favorite color."
+print("Blue" in someTxt)
+a=200
+b=200
+if b==a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+print(bool(0), bool(""), bool(None))
+def func():
+  return True
+if not func():
+  print("Yes")
+else:
+  print("No")
+# Arithmetic Operators: +,-,*,/,** (exponent),% (remainder),//
+# Comparison Operators: >,<,==,>=,<=,!=
+# Membership Operators: in, not in
 
+
+
+
+
+#My free time code
+'''
+password=input("Please type your name: ")
+
+print("Hello " + password)
+
+anything=input("Type your age: ")
+months=int(anything)*12
+print("You are " + str(months) + " months old")
+'''
