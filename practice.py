2880fbce20e2ceaa6bf45d3bf2dc85c55c3219fc
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2022-12-08 23:00:32 +0000

    12.08 sets and loops

--- practice.py
+++ practice.py
@@ -1,30 +1,46 @@
-numList=[0,0,0,0,0,0]
-min=10000
-max=-10000
-for x in numList :
-  if (min<x):
-      print(x)
-  elif(min>x):
-    print(x, "This is the smallest so far")
-    min=x
-  if (max<x):
-    max=x
+# numList=[0,0,0,0,0,0]
+# min=10000
+# max=-10000
+# for x in numList :
+#   if (min<x):
+#       print(x)
+#   elif(min>x):
+#     print(x, "This is the smallest so far")
+#     min=x
+#   if (max<x):
+#     max=x
     
-print(min, " is the minimum, ", max, " is the maximum")
+# print(min, " is the minimum, ", max, " is the maximum")
 
-flavors = ["chocolate", "vanilla", "cookies and cream", "strawberry", "mint chocolate chip"]
+# flavors = ["chocolate", "vanilla", "cookies and cream", "strawberry", "mint chocolate chip"]
 
-flavors2 = ["chocolate", "vanilla", "cookie dough", "strawberry", "cookies and cream"]
+# flavors2 = ["chocolate", "vanilla", "cookie dough", "strawberry", "cookies and cream"]
 
-print("chocolate" in flavors2)
-print("vanilla" in flavors2)
-print("cookies and cream" in flavors2)
-print("strawberry" in flavors2)
-print("mint chocolate chip" in flavors2)
+# print("chocolate" in flavors2)
+# print("vanilla" in flavors2)
+# print("cookies and cream" in flavors2)
+# print("strawberry" in flavors2)
+# print("mint chocolate chip" in flavors2)
 
-flavors.append(flavors2[2])
-print(flavors)
+# flavors.append(flavors2[2])
+# print(flavors)
 
-flavors = flavors + flavors2
-flavors.remove("mint chocolate chip")
-print(flavors)
+# flavors = flavors + flavors2
+# flavors.remove("mint chocolate chip")
+# print(flavors)
+
+
+# subjects=["english","math","history","art","psychology"]
+# for subject in subjects :
+#   length=(len(subjects))
+#   if subject=="history" :
+#     print (subject+"(U.S. history)")
+#   else :
+#     print(subject) 
+# print(length)
+
+wordList=["hippopotamus","elephant"]
+
+for x in wordList :
+  for letter in x :
+    print(letter)
